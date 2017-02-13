1f6f064bd941ade3b6d41ce2a9adcf0584e102c1
Rémi Dadvisard <remidadvisard@client-eduroam849.canalip.upmc.fr>
2017-02-13 12:19:58 +0100

    Update

---
+++ Awele/Joueurs/joueurIA.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+
+
+
+def Evaluation (jeu)
+#retourne un score d'evaluation
+
+
+
+def Estimation (jeu, coup)
+#retourne le score d'utilite pour un coup donne
+
+def Decision(jeu, coups):
+#retourne lemeilleur  coup et son score
+
+
+
+
+def saisieCoup(jeu):
